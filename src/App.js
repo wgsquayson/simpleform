58f78d99e8d50b83f5e37836196cc1ff63794192
William Quayson <wgsquayson@gmail.com>
2020-08-29 14:16:03 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Show from './icons/show.svg';
+import Hide from './icons/hide.svg';
+
 function App() {
+  const [icon, setIcon] = useState(Show);
+  const [type, setType] = useState('password');
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleVisibility() {
+    if(isVisible === false) {
+      setIcon(Hide);
+      setType('text');
+      setIsVisible(true);
+    } else {
+      setIcon(Show);
+      setType('password')
+      setIsVisible(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>wow este formulário é incrivel!!!!!</p>
+      <form className="form" action="">
+        <input 
+          className="input" 
+          placeholder="Usuário" 
+          type="text"
+        />
+        <input 
+          className="input" 
+          placeholder="Senha"
+          type={type} 
+        />
+        <img 
+          src={icon} 
+          className="icon"
+          onClick={handleVisibility}
+        />
+        <button className="button">Login</button>
+      </form>
     </div>
   );
 }
